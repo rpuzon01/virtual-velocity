4e5ad10ea9fdc136269ffcfb075d5315d1e1980b
Ryan Riley Puzon <rrgpuzon@gmail.com>
2020-11-17 18:49:54 -0800

    clean db types and defaults

--- db/init_db.js
+++ db/init_db.js
@@ -20,30 +20,32 @@ async function buildTables() {
       firstName VARCHAR(255) NOT NULL,
       lastName VARCHAR(255) NOT NULL,
       email VARCHAR(255) UNIQUE NOT NULL,
+      imageURL TEXT DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg',
       username VARCHAR(255) UNIQUE NOT NULL,
-      password VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
       "isAdmin" BOOLEAN NOT NULL DEFAULT false
     );
     CREATE TABLE orders(
       id SERIAL PRIMARY KEY,
       status VARCHAR(255) DEFAULT 'created',
       "userId" INTEGER REFERENCES users(id),
-      "datePlaced" DATE       
+      "datePlaced" DATE DEFAULT CURRENT_DATE
     );
     CREATE TABLE products(
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) UNIQUE NOT NULL,
       description TEXT UNIQUE NOT NULL,
       price VARCHAR(255) UNIQUE NOT NULL,
-      imageURL TEXT,
-      inStock BOOLEAN NOT NULL,
+      imageURL TEXT DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg',
+      inStock BOOLEAN NOT NULL DEFAULT false,
       category VARCHAR(255) NOT NULL
     );
     CREATE TABLE order_products(
       id SERIAL PRIMARY KEY,
       "productId" INTEGER REFERENCES products(id),
       "orderId" INTEGER REFERENCES orders(id),
-      price INTEGER NOT NULL
+      price INTEGER NOT NULL,
+      quantity INTEGER NOT NULL DEFAULT 0
     );
     `);
   } catch (error) {
