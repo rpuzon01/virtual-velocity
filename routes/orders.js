cc04a1c7c25c51236c1358e87985c4a199b65804
Jon-Morgan <jonmorgan.cobb@gmail.com>
2020-11-19 22:07:30 -0800

    merge conflicts

---
+++ routes/orders.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const ordersRouter = express.Router();
+const { requireUser, isAdmin } = require("./utils");
+
+//admin?
+ordersRouter.get("/orders", requireUser, isAdmin, async (req, res, next) => {
+  try {
+    const orders = await getAllOrders();
+    res.send(orders);
+  } catch (error) {
+    next(error);
+  }
+});
+
+ordersRouter.get(
+  "/orders/cart",
+  requireUser,
+  isAdmin,
+  async (req, res, next) => {
+    try {
+      const orders = await getPendingOrdersByUsers();
+      res.send(orders);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+ordersRouter.post("/orders", requireUser, async (req, res, next) => {
+  try {
+    const orders = await createNewOrder(productId, orderId, price, quantity);
+    res.send(orders);
+  } catch (error) {
+    next(error);
+  }
+});
+
+ordersRouter.get(
+  "/users/:userId/orders",
+  requireUser,
+  async (req, res, next) => {
+    try {
+      const orders = await getOrderByUser(username);
+      res.send(orders);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+module.exports = ordersRouter;
