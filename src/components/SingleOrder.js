e027e4eaf63432aa5dddb31aedca67419eb32e55
Ryan Riley Puzon <rrgpuzon@gmail.com>
2020-11-19 23:24:57 -0800

    starting useeffect

--- src/components/SingleOrder.js
+++ src/components/SingleOrder.js
@@ -4,24 +4,6 @@ import { useParams } from 'react-router-dom';
 const SingleOrder = () => {
     const [order, setOrder] = useState({});
 
-    useEffect(() => {
-        //acts as dummy data
-        setOrder({
-            id: 1,
-            status: 'created',
-            userId: 1,
-            datePlaced: "December 5, 2020",
-            products: [{
-                id: 1,
-                name: "PSA grade 10 Base set Charzard",
-                description: "(dummy description)",
-                price: 3999.95,
-                imageURL: "",
-                inStock: true,
-                category: "Pokemon Cards",
-            }]
-        })
-    }, [])
 
     return (
         <>
