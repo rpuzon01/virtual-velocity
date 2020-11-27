54ddf3fbbf80fb68157c256e4f7714873c8a5907
samuel mckaig <bboysamuel@hotmail.com>
2020-11-26 20:53:02 -0800

    cleaned up code

--- src/components/SingleOrder.js
+++ src/components/SingleOrder.js
@@ -6,80 +6,36 @@ import './SingleOrder.css'
 const SingleOrder = (props) => {
     const { orderId, } = useParams();
     const { user, orders, setOrders, token } = props;
-    // const [orders, setOrders] = useState({})
-    // const [order, setOrder] = useState('')
      const [order, setOrder] = useState({})
-    // const [order, setOrder] = useState({
-    //         id: 33,
-    //         status: 'created',
-    //         userId: 21,
-    //         datePlaced: "January 1, 2000",
-    //         products: [{
-    //             id: 22,
-    //             name: "PSA grade 10 Base set Blastoise",
-    //             description: "(dummy description)",
-    //             price: 349995,
-    //             inStock: true,
-    //             category: "Pokemon Cards",
-    //             quantity: 1
-    //         }]
-    // });
 
-    console.log('user', user)
-    console.log('userId!', user.id)
-    console.log('token', token)
-    // console.log('products', products)
+    // console.log('user', user)
+    // console.log('userId!', user.id)
+    // console.log('token', token)
 
-    console.log("order in single order2", order)
-    console.log("order id", orderId)
+    // console.log("order in single order2", order)
+    // console.log("order id", orderId)
 
-    // const handleGetOrders = async() => {
-    //     try {
-    //         const orderstest = await getOrdersByUserId(user.id, token)
-    //         console.log("orderstest", orderstest)
-
-    //     } catch (error) {
-    //         throw error
-    //     }
-    // }
-    // handleGetOrders()
-// const orderProducts = orders.map(function({products}) {
-//     console.log('ordermap#', products)
-//     return products
-// })
-
-    // console.log("ORDERst", orders)
-    // console.log("ORDER.products", orders.products)
-
-    // useEffect(() => {
-    //     getOrdersByUserId(user.id, token).then(setOrders)
-    //     console.log("order in single order", orders)
-    // }, [])
-
-    console.log("ORDERst", orders)
-    // console.log("ORDERs.products", orders[1].products)
-    {console.log('12345', orders.userId)}
+    // console.log("ORDERS in Single Order", orders)
 
 
     return ( <>
         <div className="single-order">
-            <header className="order-info">
-                <div> Order #{orders.id} </div>
-                <div> Order Status: {orders.status} </div>
-                <div> Order Placed: {orders.datePlaced} </div>
-            </header>
             <div className="products">
-            <h1>TEST: </h1>
-                { orders && orders.map(({products, id}) =>
+            <h1>My Order: </h1>
+                { orders && orders.map(({products, id, userId, datePlaced, status}) =>
                  <div key={id} className="orders">
-                    <h1> prod tes: {products.name}</h1>
-                    {console.log('12345', orders.userId)}
-                    {console.log('products in orders map', products)}
+                    {/* {console.log('12345', orders.userId)}
+                    {console.log('products in orders map', products)} */}
+                    <header className="order-info">
+                        <div> Order #{id} </div>
+                        <div> Order Status: {status} </div>
+                        <div> Order Placed: {datePlaced} </div>
+                    </header>
 
-                        { products && products.map(({id, name, description, category, price, quantity}) => <>
-                        {console.log('map data 23', id, name, description, category, price, quantity)}
+                        { products && products.map(({id, name, description, category, price, quantity}) => user && user.id === userId && <>
+                        {/* {console.log('map data 23', id, name, description, category, price, quantity)} */}
 
-                        <h1>TEST: {name}</h1>
+                        <h3>Product: {name}</h3>
                             <div key={id} className="product">
                                 <div>Name: {name}</div>
                                 <div>Category: {category}</div>
