8b86523f1969887e1c67a89418854fcb714da62c
Ryan Riley Puzon <36114210+rpuzon01@users.noreply.github.com>
2020-11-28 19:28:27 -0800

    Update utils.js

--- db/utils.js
+++ db/utils.js
@@ -1,13 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
   ...require("./products"),
   ...require("./orders"),
   ...require("./users"),
   ...require("./order_products"),
 };
-=======
-    ...require('./products'),
-    ...require('./users'),
-    ...require('./orders'),
-}
->>>>>>> 6a53fb92c576b571452f5353bc5dd6c33f2e236e
