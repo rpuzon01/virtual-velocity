aef785ee88961175f0c2eaebfbcff244bbcc82a0
Ryan Riley Puzon <rrgpuzon@gmail.com>
2020-11-30 19:37:13 -0800

    remove client error event handler

--- db/index.js
+++ db/index.js
@@ -4,10 +4,6 @@ const DB_NAME = "localhost:5432/graceshopper";
 const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
 const client = new Client(DB_URL);
 
-client.on('error', err => {
-    console.error("here's your fucking error", err);
-});
-
 module.exports = {
   client
 };
