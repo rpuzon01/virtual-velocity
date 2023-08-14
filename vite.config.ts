16a6a2a4f274a62ef6e12248a38cad5d2acf0b5e
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-08-14 16:33:51 -0700

    adds proxying for api requests into correct localhost port

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+//
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3001",
+      '/api': {
+        target: 'http://localhost:3000/api',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/fallback/, ''),
-      }
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     }
   }
-})
+});
