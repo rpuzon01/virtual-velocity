d7b4b021058d11b303299fb6e3bb0105452a9448
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-08-17 23:23:44 -0700

    adds proper status codes for being unauthorized or unauthorized within routes

--- routes/utils.js
+++ routes/utils.js
@@ -1,9 +1,9 @@
 function requireUser(req, res, next) {
   if (!req.user) {
-    next({
-      name: "MissingUserError",
-      message: "You must be logged in to perform this action!",
-    });
+    res.status(401).send({
+      name: 'UnauthenticatedUserError',
+      message: 'You must be logged in to perform this action!',
+    })
   }
 
   next();
@@ -11,10 +11,10 @@ function requireUser(req, res, next) {
 
 function isAdmin(req, res, next) {
   if (!req.user.isAdmin) {
-    next({
-      name: "UnauthenticatedUserError",
-      message: "You must be an admin to perform this action",
-    });
+    res.status(403).send({
+      name: 'UnauthorizedUserError',
+      message: 'You must be an admin to perform this action',
+    })
   }
 
   next();
