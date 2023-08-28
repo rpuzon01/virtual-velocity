762520eb6d362e15c16b4c68419a33cc89e5d660
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-08-28 16:27:13 -0500

    removes payment intents from orders router

--- routes/orders.js
+++ routes/orders.js
@@ -12,29 +12,6 @@ const {
   getOrderById
 } = require('../db/utils');
 
-const calculateTotal = (products) => {
-  let totalSum = 0;
-  for (let i = 0; i < products.length; i++) {
-    totalSum += products[i].price * 1 * products[i].quantity * 1;
-  }
-  return totalSum;
-};
-
-ordersRouter.post('/create-payment-intents', async (req, res, next) => {
-  const { products } = req.body;
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateTotal(products),
-    currency: 'USD',
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
-
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
-
 ordersRouter.get('/', [requireUser, isAdmin], async (req, res, next) => {
   try {
     const orders = await getAllOrders();
