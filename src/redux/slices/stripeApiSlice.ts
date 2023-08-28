c2f7848294452acd0d885bc626887db57b4bb78a
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-08-28 16:32:31 -0500

    add stripesessionresponse type

--- src/redux/slices/stripeApiSlice.ts
+++ src/redux/slices/stripeApiSlice.ts
@@ -1,9 +1,13 @@
 import { Order } from '../../types';
 import apiSlice from './apiSlice';
 
+type StripeSessionResponse = {
+  URL: string
+}
+
 export const stripeApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
-    createStripeSession: builder.mutation<string, Order>({
+    createStripeSession: builder.mutation<StripeSessionResponse, Order>({
       query: (order) => ({
         url: '/stripe/create-checkout-session',
         method: 'POST',
