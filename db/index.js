4397878b336415ce9395ab837e394ad264ad8a14
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-07 17:51:02 -0700

    sets ssl true

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 // Connect to DB
 const { Client } = require('pg');
 
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/graceshopper?sslmode=disable');
+const client = new Client({
+	connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/graceshopper?sslmode=disable',
+  ssl: {rejectUnauthorized: false}
+});
 
 module.exports = {
   client
