716dd8165b4db9dec47ebff8bbdff987de016671
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-07 17:49:43 -0700

    uses string client constructor for db connection

--- db/index.js
+++ db/index.js
@@ -1,9 +1,7 @@
 // Connect to DB
 const { Client } = require('pg');
 
-const client = new Client({
-	connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/graceshopper?sslmode=disable',
-});
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/graceshopper?sslmode=disable');
 
 module.exports = {
   client
